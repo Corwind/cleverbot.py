a90bd4ca3c9d8eca556557d59f1afaa2d61ed779
Guillaume Doré <dore_g@epita.fr>
2015-12-07 17:07:50 +0100

    moved cleverbot to cleverbot3 to avoid confusion

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
 from distutils.core import setup
 
 setup(
-    name = 'cleverbot',
-    packages = ['cleverbot'],
+    name = 'cleverbot3',
+    packages = ['cleverbot3'],
     version = '0.1.5',
     author = 'Rodney Folz',
     author_email = 'folz@rodneyfolz.com',
     url = 'https://github.com/folz/cleverbot.py',
-    description = 'An API for Cleverbot in Python',
+    description = 'An API for Cleverbot in Python 3',
     classifiers = [
         'License :: OSI Approved :: BSD License',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
         'Operating System :: OS Independent'
     ]
 )
