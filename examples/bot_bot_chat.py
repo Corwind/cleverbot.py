272a94f5a65bddb3bc859e4e6ee21a47375e251e
Guillaume Doré <dore_g@epita.fr>
2015-12-07 17:03:14 +0100

    port bot to bot example to python 3

--- examples/bot_bot_chat.py
+++ examples/bot_bot_chat.py
@@ -9,13 +9,13 @@ def main():
     cleverbot_client_one = cleverbot.Cleverbot()
     cleverbot_client_two = cleverbot.Cleverbot()
 
-    print '>> Cleverbot #1: Hi.'
+    print('>> Cleverbot #1: Hi.')
     answer = cleverbot_client_two.ask('Hi.')
 
     while True:
-        print '>> Cleverbot #2: {}'.format(answer)
+        print('>> Cleverbot #2: {}'.format(answer))
         answer = cleverbot_client_one.ask(answer)
-        print '>> Cleverbot #1: {}'.format(answer)
+        print('>> Cleverbot #1: {}'.format(answer))
         answer = cleverbot_client_two.ask(answer)
 
 
@@ -23,6 +23,6 @@ if __name__ == '__main__':
     try:
         main()
     except KeyboardInterrupt:
-        print '>> Exiting...'
-    except Exception, err:
-        print traceback.format_exc(err)
+        print('>> Exiting...')
+    except Exception as err:
+        print(traceback.format_exc(err))
