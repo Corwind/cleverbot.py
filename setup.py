5407114912c32bcd734ade877bec68e3a293b1a9
Guillaume Doré <dore_g@epita.fr>
2016-03-24 11:20:02 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -8,9 +8,8 @@ setup(
     author_email = 'guillaume@dore-net.fr',
     url = 'https://github.com/Corwind/cleverbot3.py',
     description = 'An API for Cleverbot in Python 3',
-    license = 'Beerware',
+    license = 'Beer-Ware',
     classifiers = [
-        'License :: Beerware',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
         'Operating System :: OS Independent'
