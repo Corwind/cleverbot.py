12ad33b3da346d2e114755d50188e98262808dd8
Guillaume Doré <dore_g@epita.fr>
2016-03-24 11:23:13 +0100

    bump version to 0.1.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name = 'cleverbot3',
     packages = ['cleverbot3'],
-    version = '0.1.1',
+    version = '0.1.2',
     author = 'Guillaume Doré',
     author_email = 'guillaume@dore-net.fr',
     url = 'https://github.com/Corwind/cleverbot3.py',
